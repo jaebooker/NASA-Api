b92028f79f64b66084d5308aee15841c6788ed2d
jaebooker <jaesonbooker@gmail.com>
2019-10-19 14:59:18 -0700

    added a few more data apis

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,39 @@ module.exports = function(app) {
     res.sendStatus(200);
   });
 
+  app.get('/exoplanet', function(req, res){
+	  const request = require('request');
+
+  request('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,ra,dec&order=dec&format=ascii', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+	console.log(body);
+    console.log(body.url);
+    console.log(body.explanation);
+  });
+    res.sendStatus(200);
+  });
+  app.get('/air', function(req, res){
+	  const request = require('request');
+
+  request('https://airnow.zendesk.com/hc/en-us/articles/212303177-How-does-AirNow-make-the-Current-PM-Air-Quality-Index-AQI-maps-', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+	console.log(body)
+    console.log(body.url);
+    console.log(body.explanation);
+  });
+    res.sendStatus(200);
+  });
+
+  app.get('/globe', function(req, res){
+	  const request = require('request');
+
+  request('https://api.globe.gov/search/v1/measurement/protocol/measureddate/?protocols=aerosols&startdate=2010-01-01&enddate=2012-01-02&geojson=FALSE&sample=FALSE', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+	console.log(body)
+    console.log(body.url);
+    console.log(body.explanation);
+  });
+    res.sendStatus(200);
+  });
+
 };
